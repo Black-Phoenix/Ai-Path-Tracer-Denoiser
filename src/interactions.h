1baf5bfda898258b1721ab1fe618a18545e9bba9
Dewang Sultania <dewangsultania@gmail.com>
2019-11-14 19:35:44 -0500

    Dielectric

--- src/interactions.h
+++ src/interactions.h
@@ -3,6 +3,7 @@
 #include "intersections.h"
 #define MESH_NORMAL_VIEW false
 #define FRESNELS true
+#define DIELECTRIC true
 
 // CHECKITOUT
 /**
@@ -83,12 +84,87 @@ bool refract(const glm::vec3& v, const glm::vec3& n, float ni_over_nt, glm::vec3
 }
 
 
+__host__ __device__ float FresnelDielectric_Evaluate(float cosThetaI, float etaI, float etaT)
+{
+	cosThetaI = glm::clamp(cosThetaI, -1.0f, 1.0f);
+
+	bool entering = cosThetaI > 0.0f;
+	float etaIb = etaI;
+	float etaTb = etaT;
+	if (!entering) {
+		etaIb = etaT;
+		etaTb = etaI;
+		cosThetaI = glm::abs(cosThetaI);
+	}
+
+	float sinThetaI = glm::sqrt(glm::max(0.0f, 1 - cosThetaI * cosThetaI));
+	float sinThetaT = etaIb / etaTb * sinThetaI;
+
+	if (sinThetaT >= 1) {
+		return 1.0f;
+	}
+
+	float cosThetaT = glm::sqrt(glm::max(0.0f, 1 - sinThetaT * sinThetaT));
+
+	float Rparl = ((etaTb * cosThetaI) - (etaIb * cosThetaT)) /
+		((etaTb * cosThetaI) + (etaIb * cosThetaT));
+	float Rperp = ((etaIb * cosThetaI) - (etaTb * cosThetaT)) /
+		((etaIb * cosThetaI) + (etaTb * cosThetaT));
+	return (Rparl * Rparl + Rperp * Rperp) / 2;
+}
 __host__ __device__ __inline__ float schlick(float cosine, float ref_idx) {
 	float r0 = (1 - ref_idx) / (1 + ref_idx); // ref_idx = n2/n1
 	r0 = r0 * r0;
 	return r0 + (1 - r0) * pow((1 - cosine), 5);
 }
+__host__ __device__ void SpecularReflection_BxDF(PathSegment & pathSegment, glm::vec3 intersect, glm::vec3 normal, const Material &m) {
+	pathSegment.color *= m.specular.color;
+	pathSegment.ray.direction = glm::reflect(pathSegment.ray.direction, normal);
+	pathSegment.ray.origin = intersect + (.001f) * pathSegment.ray.direction;
+}
 
+__host__ __device__ void SpecularRefraction_BxDF(PathSegment & pathSegment, glm::vec3 intersect, glm::vec3 normal, const Material &m, thrust::default_random_engine &rng) {
+	thrust::uniform_real_distribution<float> u01(0, 1);
+
+	// flip the normal and adjust the eta if we are leaving the  object
+	glm::vec3 wo = pathSegment.ray.direction;
+	bool leaving = glm::dot(wo, normal) > 0.f;
+	glm::vec3 n = normal * (leaving ? -1.f : 1.f);
+	float eta = leaving ? m.indexOfRefraction : (1.f / m.indexOfRefraction);
+	glm::vec3 wi = glm::refract(wo, n, eta);
+
+	// handle total internal reflection
+	if (glm::length(wi) < .01f) {
+		pathSegment.color *= 0.0f;
+		wi = glm::reflect(wo, normal);
+	}
+
+	pathSegment.color *= m.specular.color;
+	pathSegment.ray.direction = wi;
+	pathSegment.ray.origin = intersect + (.001f) * pathSegment.ray.direction;
+}
+
+__host__ __device__ void Glass_BxDF(PathSegment & pathSegment, glm::vec3 intersect, glm::vec3 normal, const Material &m, thrust::default_random_engine &rng) {
+	thrust::uniform_real_distribution<float> u01(0, 1);
+	float VdotN = glm::dot(-pathSegment.ray.direction, normal);
+	bool leaving = VdotN < 0.f;
+	float eI = leaving ? m.indexOfRefraction : 1.f;
+	float eT = leaving ? 1.f : m.indexOfRefraction;
+
+	float fresnel = FresnelDielectric_Evaluate(VdotN, eI, eT) / glm::abs(VdotN);
+
+	if (u01(rng) < fresnel) {
+		SpecularReflection_BxDF(pathSegment, intersect, normal, m);
+	}
+	else {
+		SpecularRefraction_BxDF(pathSegment, intersect, normal, m, rng);
+	}
+}
+__forceinline__ __host__ __device__ void Lambert_BxDF(PathSegment & pathSegment, glm::vec3 intersect, glm::vec3 normal, const Material &m, thrust::default_random_engine &rng) {
+	pathSegment.ray.direction = calculateRandomDirectionInHemisphere(glm::normalize(normal), rng);
+	pathSegment.color *= m.color;
+	pathSegment.ray.origin = intersect + (.001f) * pathSegment.ray.direction;
+}
 
 __host__ __device__
 void scatterRay(
@@ -100,6 +176,20 @@ void scatterRay(
 	glm::vec3 color(1.0f, 1.0f, 1.0f);
 	thrust::uniform_real_distribution<float> u01(0, 1);
 	float reflective_prob = m.hasReflective;
+	if (DIELECTRIC) {
+		if (m.hasReflective > EPSILON && m.hasRefractive > EPSILON) {
+			Glass_BxDF(pathSegment, intersection.intersect, intersection.surfaceNormal, m, rng);
+		}
+		else if (m.hasReflective > EPSILON) {
+			SpecularReflection_BxDF(pathSegment, intersection.intersect, intersection.surfaceNormal, m);
+		}
+		else if (m.hasRefractive > EPSILON) {
+			SpecularRefraction_BxDF(pathSegment, intersection.intersect, intersection.surfaceNormal, m, rng);
+		}
+		else {
+			Lambert_BxDF(pathSegment, intersection.intersect, intersection.surfaceNormal, m, rng);
+		}
+	}
 	if (reflective_prob != 0 || m.hasRefractive != 0) {
 		float pdf = u01(rng), refrac_index_ratio, cosine;
 		glm::vec3 normal;
