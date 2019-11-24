f59d4d361a25a822b057371d65143ee32d0b90b4
Dewang Sultania <dewangsultania@gmail.com>
2019-11-24 12:23:26 -0500

    Gif Saving

--- pytorch_prototype/test.py
+++ pytorch_prototype/test.py
@@ -16,17 +16,21 @@ from recurrent_autoencoder_model import *
 from dataloader import *
 from loss import *
 from tensorboard import *
+import imageio
 
+#res_list.append(morphed_set[k, :, :, :])
 logger = Logger('./logs')
 device =  torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 m = find_max('../Test/RGB',1,1)
 inputs, outputs = preprocess('../Test','../Test/RGB','../Test/Depth','../Test/Albedos','../Test/Normals','../Test/GroundTruth',m)
 dataset = AutoEncoderData('../Test/RGB',inputs,outputs,(256,256),m)
-test_loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False)
 model =   AutoEncoder(10).to(device)
+fin = np.zeros((256,768,3))
 checkpoint = torch.load('autoencoder_model.pt')
 model.load_state_dict(checkpoint['net'])
 overall_step = 0
+res_list = []
 total_step = len(test_loader)
 with torch.no_grad():
     for i, data in enumerate(test_loader):
@@ -34,17 +38,24 @@ with torch.no_grad():
         output = data['output'].float().to(device)
         print(output.shape)
         for j in range(7):
-            fig, ax = plt.subplots(3)
+            fin = np.zeros((256,768,3))
+            # fig, ax = plt.subplots(3)
             input_i = input[:,j,:,:,:]
             output_i = output[:,j,:,:,:]
-            ax[0].imshow(input_i[0,:3,:,:].permute(1,2,0).detach().cpu().numpy())
-            ax[0].set_title("Noisy Image")
+            fin[:,:256,:] = input_i[0,:3,:,:].permute(1,2,0).detach().cpu().numpy()
+            # ax[0].imshow(fin[:,:256,:])
+            # ax[0].set_title("Noisy Image")
             model.set_input(input_i)
             if j == 0:
                 model.reset_hidden()
             pred = model()
-            ax[1].imshow(pred[0].permute(1,2,0).cpu().numpy())
-            ax[1].set_title("Denoised Image")
-            ax[2].imshow(output_i[0,:,:,:].permute(1,2,0).detach().cpu().numpy())
-            ax[2].set_title("Ground Truth")
-            plt.show()
+            fin[:,256:512,:]  = pred[0].permute(1,2,0).cpu().numpy()
+            # ax[1].imshow(fin[:,256:512,:])
+            # ax[1].set_title("Denoised Image")
+            fin[:,512:,:] = output_i[0,:,:,:].permute(1,2,0).detach().cpu().numpy()
+            # ax[2].imshow(fin[:,512:,:])
+            # ax[2].set_title("Ground Truth")
+            # plt.show()
+            res_list.append(fin)
+        break
+imageio.mimsave('./eval_testimg.gif', res_list)
