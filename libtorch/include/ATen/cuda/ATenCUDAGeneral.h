6f86de2bd2739c3e93fe4fe9c2aaae04b06ce777
Vaibhav Arcot <yvarcot@seas.upenn.edu>
2019-12-08 17:26:17 -0500

    Clean up

--- libtorch/include/ATen/cuda/ATenCUDAGeneral.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cuda.h>
-#include <cuda_runtime.h>
-#include <cuda_fp16.h>
-
-#include <c10/macros/Export.h>
-
-#define AT_CUDA_API CAFFE2_API
--- src/preview.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-extern GLuint pbo;
-
-std::string currentTimeString();
-bool init();
-void mainLoop();
